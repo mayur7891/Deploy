89c72e5dab1b4d1d7a1330ba37184e6245d924c8
Sahil Sharma <sharma2003sahil8657@gmail.com>
2025-02-28 22:31:15 +0530

    environment variables

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
+# import os
+
+# class Config:
+#     """Configuration settings for the Flask app."""
+#     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+#     JWT_SECRET_KEY = "your_jwt_secret_key_here"
+#     MONGO_URI = "mongodb+srv://Ash781:mayur123@dummy.76pbm.mongodb.net/dummy?retryWrites=true&w=majority"
+
 import os
 
 class Config:
     """Configuration settings for the Flask app."""
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    JWT_SECRET_KEY = "your_jwt_secret_key_here"
-    MONGO_URI = "mongodb+srv://Ash781:mayur123@dummy.76pbm.mongodb.net/dummy?retryWrites=true&w=majority"
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")  # Default for local dev
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret_key_here")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Ash781:mayur123@dummy.76pbm.mongodb.net/dummy?retryWrites=true&w=majority")  # Default local MongoDB
